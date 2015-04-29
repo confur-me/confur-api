ea82596326959008de71d2e772cc597d766af7d7
Sergey Besedin <kr3ssh@gmail.com>
2015-04-30 01:26:58 +0300

    test seeds

--- test/fixtures/fixtures.go
+++ test/fixtures/fixtures.go
@@ -11,13 +11,48 @@ func Seed() {
 	if err != nil {
 		fmt.Println("Db connection error", err)
 	} else {
-		conference := models.Conference{
-			Title: "MoscowJS",
-			Url:   "http://moscowjs.ru",
-			Type:  "meetup",
+
+		tags := []models.Tag{{
+			Slug:  "go",
+			Title: "Go",
+		}, {
+			Slug:  "ruby",
+			Title: "Ruby",
+		}}
+
+		for _, tag := range tags {
+			d.Create(&tag)
 		}
 
-		d.Create(&conference)
+		conferences := []models.Conference{{
+			Title:       "MoscowJS",
+			Url:         "http://moscowjs.ru",
+			Type:        "meetup",
+			Events:      []models.Event{{Address: "Russian Federation, Moscow, Red square street."}},
+			Description: "Cum illum voluptas ducimus",
+			Videos: []models.Video{{
+				Title:   "Videooo",
+				Url:     "http://www.youtube.com/watch?v=oHg5SJYRHA0",
+				Length:  180,
+				Service: "youtube",
+				Tags:    tags,
+			}, {
+				Title:   "Videooo 2",
+				Url:     "http://www.youtube.com/watch?v=oHg5SJYRHA0",
+				Length:  120,
+				Service: "youtube",
+			}},
+		},
+			{
+				Title:  "Rails Club",
+				Url:    "http://gay.org",
+				Type:   "conference",
+				Events: []models.Event{{Address: "Russian Federation, Siberia."}},
+			}}
+
+		for _, conference := range conferences {
+			d.Create(&conference)
+		}
 	}
 
 }
